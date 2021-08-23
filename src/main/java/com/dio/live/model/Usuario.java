7cc0542d2a4bd914f86d5148876e2895247c3be0
wwfia <Esse@e00>
2021-08-23 17:33:35 -0300

    Initial and last commit

---
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -0,0 +1,37 @@
+package com.dio.live.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+@Audited
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    private CategoriaUsuario categoriaUsuario;
+    private String nome;
+    @ManyToOne
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+    private BigDecimal tolerancia;
+    private LocalDateTime finalJornada;
+}
